cd0e00fe472be8e8f76090a8a8ac71d3366a2938
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-01 12:10:11 -0500

    added scrip

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assigned Variables
+var startButton = document.querySelector(".start-btn")
+var startScreen = document.querySelector("#beginQuiz")
+var questions = [
+    q1,
+    q2,
+    q3,
+    q4,
+    q5
+]
+var q1 = document.querySelector("#q1")
+var q2 = document.querySelector("#q2")
+var q3 = document.querySelector("#q3")
+var q4 = document.querySelector("#q4")
+var q5 = document.querySelector("#q5")
+var score = 0
+var timeLeft = 30
+var results = document.querySelector("#results")
+var timer = document.querySelector("#timer")
+var doneQuiz = false
